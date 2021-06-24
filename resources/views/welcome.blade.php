318ff2dd56fd90f1a7ad11d970fe375ef586b98d
dewanmahdi <dewanmahdi@gmail.com>
2021-06-24 21:54:03 +0600

    First

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,124 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Wise Gamers</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="/assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload landing">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header" class="sticky-top">
+					<h1 id="logo"><a href="https://www.youtube.com/watch?v=qIcTM8WXFjk">You're Breathtaking!</a></h1>
+					<nav id="nav">
+						<ul>
+							<li><a href="{{ 'login' }}" class="button angry">Log in</a></li>
+                            <li><a href="{{ 'register' }}" class="button primary">Sign up</a></li>
+
+
+
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Banner -->
+				<section id="banner">
+
+					<div class="content">
+						<header>
+							<h2>A place for wise Gamers!</h2>
+							<p>See the reviews before playing<br />
+							A place only for gamers, by the gamers!</p>
+						</header>
+
+					</div>
+					<a href="#one" class="goto-next scrolly">Next</a>
+				</section>
+
+			<!-- One -->
+				<section id="one" class="spotlight style1 bottom">
+					<span class="image fit main"><img src="/images/pic02.jpg" alt="" /></span>
+					<div class="content">
+						<div class="container">
+							<div class="row">
+								<div class="col-4 col-12-medium">
+									<header>
+										<h2>Best 2 Games of 2019 so far</h2>
+										<p>Here are the review of best two games in my point of view </p>
+									</header>
+								</div>
+								<div class="col-4 col-12-medium">
+									<p>Lots of new games are getting released, so much goodworks by gaming houses, but in my list I will put two games ahead of every other games released this year</p>
+								</div>
+
+							</div>
+						</div>
+					</div>
+					<a href="#two" class="goto-next scrolly">Next</a>
+				</section>
+
+			<!-- Two -->
+				<section id="two" class="spotlight style2 right">
+					<span class="image fit main"><img src="/images/pic03.jpg" alt="" /></span>
+					<div class="content">
+						<header>
+							<h2>Metro Exodus</h2>
+							<p>This game created the biggest hype for the fans of "Metro" series</p>
+						</header>
+						<p>Metro Exodus is an epic, story-driven first person shooter from 4A Games that blends deadly combat and stealth with exploration and survival horror in one of the most immersive game worlds ever created.</p>
+						<ul class="actions">
+							<li><a href="https://www.metrothegame.com/en-gb/" class="button">Learn More</a></li>
+						</ul>
+					</div>
+					<a href="#three" class="goto-next scrolly">Next</a>
+				</section>
+
+			<!-- Three -->
+				<section id="three" class="spotlight style3 left">
+					<span class="image fit main bottom"><img src="/images/pic04.jpg" alt="" /></span>
+					<div class="content">
+						<header>
+							<h2>Star Wars Jedi: Fallen Order</h2>
+							<p>yet to release but already won every hearts!</p>
+						</header>
+						<p>Star Wars Jedi: Fallen Order is an upcoming single-player action-adventure video game developed by Respawn Entertainment and published by Electronic Arts, set in the Star Wars universe shortly after Episode III – Revenge of the Sith</p>
+						<ul class="actions">
+							<li><a href="https://www.ea.com/en-gb/games/starwars/jedi-fallen-order" class="button">Learn More</a></li>
+						</ul>
+					</div>
+
+				</section>
+
+
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+
+						<li><a href="https://www.facebook.com/Dewanmahdi" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+						<li><a href="https://www.instagram.com/dewanmahdi/" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="https://github.com/dewanmahdi" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; All rights reserved 2019.</li><li>Design: <a href="https://www.facebook.com/Dewanmahdi">Dewan Osman Mahdi </a></li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="/assets/js/jquery.min.js"></script>
+			<script src="/assets/js/jquery.scrolly.min.js"></script>
+			<script src="/assets/js/jquery.dropotron.min.js"></script>
+			<script src="/assets/js/jquery.scrollex.min.js"></script>
+			<script src="/assets/js/browser.min.js"></script>
+			<script src="/assets/js/breakpoints.min.js"></script>
+			<script src="/assets/js/util.js"></script>
+			<script src="/assets/js/main.js"></script>
+
+	</body>
+</html>
